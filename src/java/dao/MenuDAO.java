e05115617133f1cd5d47b5ea29a3616edb0b59fd
alicimsamil <81926983+alicimsamil@users.noreply.github.com>
2022-04-07 01:05:40 +0300

    create menu and room database access object

---
+++ src/java/dao/MenuDAO.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import entity.Menu;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import util.DBConnection;
+
+/**
+ *
+ * @author Ali
+ */
+public class MenuDAO {
+
+    private Connection db;
+
+    public void createMenu(Menu menu) {
+        try {
+            Statement st = this.getDb().createStatement();
+            String query = "intert into menu (food,drink,desserts) values('" + menu.getFood() + "','" + menu.getDrink() + "','" + menu.getDesserts() + ")";
+            int r = st.executeUpdate(query);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public List<Menu> getAllMenus() {
+        List<Menu> menus = new ArrayList<>();
+        try {
+            Statement st = this.getDb().createStatement();
+            String query = "select * from menu";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                menus.add(new Menu(rs.getLong("id"), rs.getString("food"), rs.getString("drink"), rs.getString("desserts")));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return menus;
+    }
+
+    public Menu getMenu(int id) {
+        Menu menu = null;
+        try {
+            Statement st = this.getDb().createStatement();
+            String query = "select * from menu where id='" + id + "' limit 1";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                menu = new Menu(rs.getLong("id"), rs.getString("food"), rs.getString("drink"), rs.getString("desserts"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return menu;
+    }
+
+    public Connection getDb() {
+        if (this.db == null) {
+            DBConnection dbc = new DBConnection();
+            this.db = dbc.connect();
+        }
+        return db;
+    }
+
+    public void setDb(Connection db) {
+        this.db = db;
+    }
+
+}
---
+++ src/java/dao/RoomDAO.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import entity.Room;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import util.DBConnection;
+
+/**
+ *
+ * @author Ali
+ */
+public class RoomDAO {
+
+    private Connection db;
+
+    public void createRoom(Room room) {
+        try {
+            Statement st = this.getDb().createStatement();
+            String query = "intert into room (roomno,bednumber,location,isfull) values('" + room.getRoomNo() + "','" + room.getBedNumber() + "','" + room.getLocation() + "','" + room.getIsFull() + ")";
+            int r = st.executeUpdate(query);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public List<Room> getAllRooms() {
+        List<Room> rooms = new ArrayList<>();
+        try {
+            Statement st = this.getDb().createStatement();
+            String query = "select * from room";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                rooms.add(new Room(rs.getLong("id"), rs.getLong("roomNo"), rs.getLong("bedNumber"), rs.getString("location"), rs.getBoolean("isFull")));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return rooms;
+    }
+
+    public Room getRoom(int roomNo) {
+        Room room = null;
+        try {
+            Statement st = this.getDb().createStatement();
+            String query = "select * from room where roomno='" + roomNo + "' limit 1";
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                room = new Room(rs.getLong("id"), rs.getLong("roomNo"), rs.getLong("bedNumber"), rs.getString("location"), rs.getBoolean("isFull"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return room;
+    }
+
+    public Connection getDb() {
+        if (this.db == null) {
+            DBConnection dbc = new DBConnection();
+            this.db = dbc.connect();
+        }
+        return db;
+    }
+
+    public void setDb(Connection db) {
+        this.db = db;
+    }
+}
